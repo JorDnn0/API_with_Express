740df1f832333e22d276d63999ec5251166862ad
JorDnn0 <90001659+JorDnn0@users.noreply.github.com>
2023-07-03 11:15:36 +0100

    Rovers Get request response

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express from "express";
+import express, {response} from "express";
+
+//env setup
+require('dotenv').config()
 
 const app = express();
 const port = 8000;
+
 
 app.use(express.json());
 const router = express.Router();
@@ -11,4 +15,27 @@ app.use('/', router);
 app.listen(port, () => {
     console.log(`Test backend is running on port ${port}`);
 });
+app.get("/rovers", function (req, res) {
+
+    let responseData = httpGet("https://api.nasa.gov/mars-photos/api/v1/rovers?api_key="+process.env.API_KEY)
+    res.json(responseData);
+
+})
+
+
+
+function httpGet(url: string)  {
+    fetch(url).then(response => response.json())
+        .then(data => {
+            console.log(data)
+            return data
+        })
+        .catch(error => {
+            // Handle errors
+            console.log(url)
+            console.error(error)
+        })
+}
+
+
 
